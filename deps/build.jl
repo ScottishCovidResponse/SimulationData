69843bbfe617faabfde210bfae4e1b00ac3f324d
Alex Robson <alex.robson@invenialabs.co.uk>
2020-06-24 11:44:44 +0100

    Update deps to build if CI is seen

--- deps/build.jl
+++ deps/build.jl
@@ -1,1 +1,12 @@
-run(`git clone --depth 1 https://github.com/ScottishCovidResponse/temporary_data`)
+"""
+    env_bool(key)
+
+Checks for an enviroment variable and fuzzy converts it to a bool
+"""
+env_bool(key, default=false) = haskey(ENV, key) ? lowercase(ENV[key]) ∉ ["0","","false", "no"] : default
+
+
+if env_bool("TRAVIS_CI_BUILD")
+    println("Downloading temporary data")
+    run(`git clone --depth 1 https://github.com/ScottishCovidResponse/temporary_data`)
+end
