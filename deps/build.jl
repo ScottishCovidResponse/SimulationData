ef976313c3b2af6578849299134cd7eff599024d
Alex Robson <alex.robson@invenialabs.co.uk>
2020-06-28 16:30:34 +0100

    Invert boolean

--- deps/build.jl
+++ deps/build.jl
@@ -6,7 +6,7 @@ Checks for an enviroment variable and fuzzy converts it to a bool
 env_bool(key, default=false) = haskey(ENV, key) ? lowercase(ENV[key]) ∉ ["0","","false", "no"] : default
 
 
-if env_bool("TRAVIS_CI_BUILD")
+if !env_bool("TRAVIS_CI_BUILD")
     println("Downloading temporary data")
     run(`git clone --depth 1 https://github.com/ScottishCovidResponse/temporary_data`)
 end
