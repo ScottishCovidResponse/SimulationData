003c888e998fbf173994d4f599d3b73e46ea81b2
Alex Robson <alex.robson@invenialabs.co.uk>
2020-07-05 18:32:36 +0100

    Use Anaconda Cloud Conda package

--- deps/build.jl
+++ deps/build.jl
@@ -6,14 +6,4 @@ using Conda
 Checks for an enviroment variable and fuzzy converts it to a bool
 """
 env_bool(key, default=false) = haskey(ENV, key) ? lowercase(ENV[key]) ∉ ["0","","false", "no"] : default
-
-# Clone the Python DATA API package
-if !isdir("data_pipeline_api")
-    @info "Downloading data_pipeline_api"
-    #run(`git clone https://github.com/ScottishCovidResponse/data_pipeline_api`)
-    pip = joinpath(Conda.BINDIR, "pip")
-    run(`$pip install pyyaml setuptools-scm`)
-    run(`$pip install git+https://github.com/ScottishCovidResponse/data_pipeline_api@ar/setup_file_formats`)
-else
-    @info "data_pipeline_api already found."
-end
+Conda.add("data_pipeline_api"; channel="scottishcovidresponse")
