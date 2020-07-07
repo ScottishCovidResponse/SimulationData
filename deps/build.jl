39a3add25f28e8db2553b51b9c927406b03e931d
Alex Robson <alex.robson@invenialabs.co.uk>
2020-07-07 10:36:40 +0100

    Update README and remove gunk from build script

--- deps/build.jl
+++ deps/build.jl
@@ -4,19 +4,6 @@ Pkg.build("PyCall") #
 
 using Conda
 
-"""
-    env_bool(key)
-
-Checks for an enviroment variable and fuzzy converts it to a bool
-"""
-env_bool(key, default=false) = haskey(ENV, key) ? lowercase(ENV[key]) ∉ ["0","","false", "no"] : default
-
-# Clone the Python DATA API package
-if !isdir("data_pipeline_api")
-    #run(`git clone https://github.com/ScottishCovidResponse/data_pipeline_api`)
-    pip = joinpath(Conda.BINDIR, "pip")
-    run(`$pip install pyyaml setuptools-scm`)
-    run(`$pip install git+https://github.com/ScottishCovidResponse/data_pipeline_api@ar/setup_file_formats`)
-else
-    @info "data_pipeline_api already found."
-end
+pip = joinpath(Conda.BINDIR, "pip")
+run(`$pip install pyyaml setuptools-scm`)
+run(`$pip install git+https://github.com/ScottishCovidResponse/data_pipeline_api@ar/setup_file_formats`)
