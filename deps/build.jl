710ffd6e146d5b4cb0940da99d131ebf93436966
Richard Reeve <git@richardreeve.net>
2020-08-11 23:38:17 +0100

    Install scipy and aiohttp

--- deps/build.jl
+++ deps/build.jl
@@ -5,5 +5,5 @@ Pkg.build("PyCall")
 using Conda
 
 pip = joinpath(Conda.BINDIR, "pip")
-run(`$pip install pyyaml setuptools-scm`)
+run(`$pip install scipy pyyaml setuptools-scm aiohttp`)
 run(`$pip install git+https://github.com/ScottishCovidResponse/data_pipeline_api@0.7.0`)
