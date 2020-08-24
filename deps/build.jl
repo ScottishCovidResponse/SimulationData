b0cf46c83d03d4464afdfdc7c1299b80ac0049da
James Cook <cookjws@gmail.com>
2020-08-24 15:54:16 +0100

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -4,6 +4,8 @@ Pkg.build("PyCall")
 
 using Conda
 
+Conda.add("pip")
+
 pip = joinpath(Conda.BINDIR, "pip")
 run(`$pip install scipy pyyaml setuptools-scm aiohttp`)
 run(`$pip install git+https://github.com/ScottishCovidResponse/data_pipeline_api@0.7.0`)
