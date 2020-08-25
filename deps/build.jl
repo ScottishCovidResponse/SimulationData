425f7dcfe18bb1f43916e0b76f15d907ac632d96
James Cook <cookjws@gmail.com>
2020-08-25 09:46:25 +0100

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -6,15 +6,16 @@ using PyCall
 
 const PACKAGES = ["scipy", "pyyaml", "setuptools-scm", "aiohttp", "git+https://github.com/ScottishCovidResponse/data_pipeline_api@0.7.0"]
 
-try
-    @pyimport pip
+pip = try
+    PyCall.pyimport("pip")
 catch
     get_pip = joinpath(dirname(@__FILE__), "get-pip.py")
     download("https://bootstrap.pypa.io/get-pip.py", get_pip)
     run(`$(PyCall.python) $get_pip --user`)
+    PyCall.pyimport("pip")
 end
 
-args = UTF8String[]
+args = String[]
 if haskey(ENV, "http_proxy")
     push!(args, "--proxy")
     push!(args, ENV["http_proxy"])
@@ -23,5 +24,4 @@ push!(args, "install")
 push!(args, "--user")
 append!(args, PACKAGES)
 
-@pyimport pip
 pip.main(args)
