3993f312193636442f3ea043cd91518e6be65941
James Cook <cookjws@gmail.com>
2020-08-25 09:17:10 +0100

    build.jl to bootstrap pip

--- deps/build.jl
+++ deps/build.jl
@@ -2,8 +2,26 @@ using Pkg
 ENV["PYTHON"] = ""
 Pkg.build("PyCall")
 
-using Conda
+using PyCall
 
-pip = joinpath(Conda.BINDIR, "pip")
-run(`$pip install scipy pyyaml setuptools-scm aiohttp`)
-run(`$pip install git+https://github.com/ScottishCovidResponse/data_pipeline_api@0.7.0`)
+const PACKAGES = ["scipy", "pyyaml", "setuptools-scm", "aiohttp", "git+https://github.com/ScottishCovidResponse/data_pipeline_api@0.7.0"]
+
+try
+    @pyimport pip
+catch
+    get_pip = joinpath(dirname(@__FILE__), "get-pip.py")
+    download("https://bootstrap.pypa.io/get-pip.py", get_pip)
+    run(`$(PyCall.python) $get_pip --user`)
+end
+
+args = UTF8String[]
+if haskey(ENV, "http_proxy")
+    push!(args, "--proxy")
+    push!(args, ENV["http_proxy"])
+end
+push!(args, "install")
+push!(args, "--user")
+append!(args, PACKAGES)
+
+@pyimport pip
+pip.main(args)
