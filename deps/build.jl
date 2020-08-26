71fac56f875cf32baa78946298594c3cff585a4f
Cook, James <james.cook@ukaea.uk>
2020-08-26 15:03:00 +0100

    reverting to trying to Conda.add("pip")

--- deps/build.jl
+++ deps/build.jl
@@ -2,25 +2,10 @@ using Pkg
 ENV["PYTHON"] = ""
 Pkg.build("PyCall")
 
-using PyCall
+using Conda
 
-const PACKAGES = ["scipy", "pyyaml", "setuptools-scm", "aiohttp", "git+https://github.com/ScottishCovidResponse/data_pipeline_api@0.7.0"]
+Conda.add("pip")
 
-pip = try
-    PyCall.pyimport("pip")
-catch
-    get_pip = joinpath(dirname(@__FILE__), "get-pip.py")
-    download("https://bootstrap.pypa.io/get-pip.py", get_pip)
-    run(`$(PyCall.python) $get_pip --user`)
-    PyCall.pyimport("pip")
-end
-
-args = String[]
-if haskey(ENV, "http_proxy")
-    push!(args, "--proxy")
-    push!(args, ENV["http_proxy"])
-end
-push!(args, "install")
-append!(args, PACKAGES)
-
-pip.main(args)
+pip = joinpath(Conda.BINDIR, "pip")
+run(`$pip install scipy pyyaml setuptools-scm aiohttp`)
+run(`$pip install git+https://github.com/ScottishCovidResponse/data_pipeline_api@0.7.0`)
