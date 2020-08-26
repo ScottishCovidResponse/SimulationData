2d2189196a8778937e36c540a4990102cb8e8b3e
Cook, James <james.cook@ukaea.uk>
2020-08-26 16:08:22 +0100

    last chance and I definitely mean it this time

--- deps/build.jl
+++ deps/build.jl
@@ -3,28 +3,7 @@ ENV["PYTHON"] = ""
 Pkg.build("PyCall")
 
 using Conda
-Conda.add("pip")
 
-using PyCall
-
-const PACKAGES = ["scipy", "pyyaml", "setuptools-scm", "aiohttp", "git+https://github.com/ScottishCovidResponse/data_pipeline_api@0.7.0"]
-
-pip = try
-    PyCall.pyimport("pip")
-catch
-    get_pip = joinpath(dirname(@__FILE__), "get-pip.py")
-    download("https://bootstrap.pypa.io/get-pip.py", get_pip)
-    run(`$(PyCall.python) $get_pip --user`)
-    PyCall.pyimport("pip")
-end
-
-args = String[]
-if haskey(ENV, "http_proxy")
-    push!(args, "--proxy")
-    push!(args, ENV["http_proxy"])
-end
-push!(args, "install")
-append!(args, PACKAGES)
-
-pip.main(args)
-
+pip = joinpath(Conda.BINDIR, "pip")
+run(`$pip install scipy pyyaml setuptools-scm aiohttp`)
+run(`$pip install git+https://github.com/ScottishCovidResponse/data_pipeline_api@0.7.0`)
