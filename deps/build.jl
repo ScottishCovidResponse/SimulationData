dd231032d7a880687fb5e47779f3e6ed7ec5d8df
Cook, James <james.cook@ukaea.uk>
2020-08-26 15:14:11 +0100

    last chance

--- deps/build.jl
+++ deps/build.jl
@@ -3,9 +3,28 @@ ENV["PYTHON"] = ""
 Pkg.build("PyCall")
 
 using Conda
-
 Conda.add("pip")
 
-pip = joinpath(Conda.BINDIR, "pip")
-run(`$pip install scipy pyyaml setuptools-scm aiohttp`)
-run(`$pip install git+https://github.com/ScottishCovidResponse/data_pipeline_api@0.7.0`)
+using PyCall
+
+const PACKAGES = ["scipy", "pyyaml", "setuptools-scm", "aiohttp", "git+https://github.com/ScottishCovidResponse/data_pipeline_api@0.7.0"]
+
+pip = try
+    PyCall.pyimport("pip")
+catch
+    get_pip = joinpath(dirname(@__FILE__), "get-pip.py")
+    download("https://bootstrap.pypa.io/get-pip.py", get_pip)
+    run(`$(PyCall.python) $get_pip --user`)
+    PyCall.pyimport("pip")
+end
+
+args = String[]
+if haskey(ENV, "http_proxy")
+    push!(args, "--proxy")
+    push!(args, ENV["http_proxy"])
+end
+push!(args, "install")
+append!(args, PACKAGES)
+
+pip.main(args)
+
